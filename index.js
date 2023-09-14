c6fa70e4e5e2a4e79b306925f2b942cffcaa33e0
miyemicodes <lizzymiyemi@gmail.com>
2023-09-14 09:03:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs",
+    {name:"Angela"});
+});
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
